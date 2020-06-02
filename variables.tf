c71a19438a089739a0b13dedac0f3ccd50e1ad09
Farkhod <sadykovfarkhod@gmail.com>
2020-06-01 19:52:23 -0500

    Fixed comments  on variable

--- variables.tf
+++ variables.tf
@@ -1,41 +1,46 @@
 ## The name of the deployment
 variable "deployment_name" {
-  description = "The name of the deployment"
+  description = "(Required) The name of the deployment"
 }
 
 ## The name of the environment
 variable "deployment_environment" {
-  description = "The name of the environment"
+  description = "(Required) The name of the environment"
 }
 
 ## Chart location or chart name
 variable "deployment_path" {
-  description = "Chart location or chart name <stable/example>"
+  description = "(Required) Chart location or chart name <stable/example>"
 }
 
 ## Endpoint for the application
 variable "deployment_endpoint" {
-  description = "Endpoint for the application"
+  description = "(Required) Endpoint for the application"
 }
 
 variable "template_custom_vars" {
   type    = "map"
   default = {}
+  description = "(Optional) A map of arbitrary strings to parse custom variables."
 }
 
 variable "env_vars" {
   type    = "map"
   default = {}
+  description = "(Optional) A map of arbitrary strings to parse helm charts"
 }
 
 variable "trigger" {
   default = "UUID"
+  description = "(Optional) A map of arbitrary strings that, when changed, will force the null resource to be replaced, re-running any associated provisioners."
 }
 
 variable "timeout" {
   default = "400"
+  description = "(Optional) Time in seconds to wait for any individual kubernetes operation (like Jobs for hooks). Defaults to 300 seconds."
 }
 
 variable "recreate_pods" {
   default = false
+  description = "(Optional) Perform pods restart during upgrade/rollback. Defaults to false"
 }
